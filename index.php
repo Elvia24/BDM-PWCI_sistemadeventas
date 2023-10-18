4b6d793493526a430f514e9945e45fa9d5ca9b2f
itsalisson <alissonsher@gmail.com>
2023-10-18 15:29:15 -0600

    parte del landing page

--- index.php
+++ index.php
@@ -3,26 +3,47 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="LPstyle.css">
   <title>Login Ventas Bisontes </title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">-->
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="public/plugins/fontawesome-free/css/all.min.css">
+   <!--<link rel="stylesheet" href="public/plugins/fontawesome-free/css/all.min.css">-->
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+   <!--<link rel="stylesheet" href="public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">-->
   <!-- viejo estilo -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+   <!--<link rel="stylesheet" href="dist/css/adminlte.min.css">-->
  <!-- nuevo estilo -->
-  <link rel="stylesheet" href="public/css/Colores.css">
-  <link rel="stylesheet" href="../prueba.scss">
+   <!--<link rel="stylesheet" href="public/css/Colores.css">-->
+   <!--<link rel="stylesheet" href="../prueba.scss">-->
 </head>
 <body class="hold-transition login-page2">
 
+<section>
+  <header>
+    <a href="#" class="logo">Ventas</a>
+    <ul class="navbar">
+      <li><a href="Login/index.php">Iniciar sesión</a></li>
+      <li><a href="Registro/Registro.php">Registro</a></li>
+</ul>
+  </header>
+
+  <div class="contenido">
+    <div class="tituloBox">
+      <h2>Explora la esencia<br></h2>
+      <h2>de nuestra<span>facultad</span></h2>
+      <p>en cada producto que ofrecemos.</p>
+      <a href="#">Comprar</a>
+</div>
+<div class="imgBox">
+</div>
+</section>
+
 <div class="login-box">
  <h1>aqui va la Landing page</h1>
-<a href="Login/index.php">Iniciar Sesion</a>
-<a href="Registro/Registro.php">Registro</a>
+
+
 
 
 </div>
@@ -35,5 +56,8 @@
 <script src="public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
