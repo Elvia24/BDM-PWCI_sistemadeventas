ce25ca24b8025f30c3def74f8f920af80e2c5d26
Elvia24 <elviaarriaga24@gmail.com>
2023-11-21 00:13:15 -0600

    ultimos retoques

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+<!-- icono de la pagina -->
+  <link rel="icon" href="http://localhost/BDM-PWCI_sistemadeventas/icon/bisonte.ico" type="image/x-icon">
+ 
   <link rel="stylesheet" href="LPstyle.css">
   <title>Login Ventas Bisontes </title>
 
@@ -18,8 +22,7 @@
    <!--<link rel="stylesheet" href="public/css/Colores.css">-->
    <!--<link rel="stylesheet" href="../prueba.scss">-->
 
-   <!-- icono de la pagina -->
-  <link rel="icon" href="<?php echo $URL;?>icon/bisonte.ico" type="image/x-icon">
+  
 </head>
 <body class="hold-transition login-page2">
 
@@ -51,11 +54,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="public/plugins/jquery/jquery.min.js"></script>
+<script src="http://localhost/BDM-PWCI_sistemadeventas/public/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="http://localhost/BDM-PWCI_sistemadeventas/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="http://localhost/BDM-PWCI_sistemadeventas/dist/js/lte.min.js"></script>
 
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
